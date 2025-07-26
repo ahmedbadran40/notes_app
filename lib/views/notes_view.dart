40da57a9a75dda9ba9827387cb3e260035e8150e
Ahmed Badran <ahmed.badran11133@gmail.com>
2025-07-26 10:59:59 +0400

    Creates edit note view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widget/add_note_buttom_sheet.dart';
-import 'package:notes_app/widget/notes_view_body.dart';
+import 'package:notes_app/widget/custom_notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
--- lib/widget/custom_note_item.dart
+++ lib/widget/custom_note_item.dart
@@ -1,47 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffffcf7f),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 26, color: Colors.black),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffffcf7f),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 26, color: Colors.black),
+              ),
 
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 16),
-              child: Text(
-                'Learn how to build beautiful apps with Flutter.',
-                style: TextStyle(fontSize: 20, color: Colors.black54),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+                child: Text(
+                  'Learn how to build beautiful apps with Flutter.',
+                  style: TextStyle(fontSize: 20, color: Colors.black54),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(FontAwesomeIcons.trash, color: Colors.black, size: 24),
-            ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May25,2025',
-              style: TextStyle(fontSize: 16, color: Colors.black54),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May25,2025',
+                style: TextStyle(fontSize: 16, color: Colors.black54),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
