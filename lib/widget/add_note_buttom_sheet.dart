26c12f953002c5f5f7f4c7fc42c70213843a5375
Ahmed Badran <ahmed.badran11133@gmail.com>
2025-07-26 08:15:31 +0400

    add border redius to bottom sheet

--- lib/widget/add_note_buttom_sheet.dart
+++ lib/widget/add_note_buttom_sheet.dart
@@ -5,6 +5,6 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(decoration: BoxDecoration());
   }
 }
