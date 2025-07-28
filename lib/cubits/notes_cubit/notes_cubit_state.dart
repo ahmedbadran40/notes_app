a88f7d2aeca56a2e908e5ede115a5892aee1b957
Ahmed Badran <ahmed.badran11133@gmail.com>
2025-07-28 05:29:44 +0400

    create notes state

---
+++ lib/cubits/notes_cubit/notes_cubit_state.dart
@@ -0,0 +1,19 @@
+part of 'notes_cubit_cubit.dart';
+
+sealed class NotesCubitState {}
+
+final class NotesCubitInitial extends NotesCubitState {}
+
+final class NotesLoading extends NotesCubitState {}
+
+final class NotesSuccess extends NotesCubitState {
+  final List<NoteModel> notes;
+
+  NotesSuccess(this.notes);
+}
+
+final class NotesFailur extends NotesCubitState {
+  final String errorrMessage;
+
+  NotesFailur(this.errorrMessage);
+}
